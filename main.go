dacf10091482bf5c98f6d4356c30f1ff17a64b99
naman mohnani <mohnaninaman@gmail.com>
2024-09-02 11:46:02 +0530

    init 2

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	key := os.Getenv("GEMINI_API_KEY")
 	fmt.Println("Hello, World!")
+	fmt.Println(key)
 }
